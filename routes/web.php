c089ccded1ee0d1bf5b50f970a4d925b552922dd
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2017-06-16 12:25:21 -0400

    cambio en la ruta

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('view', function(){
+	return view('index');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
