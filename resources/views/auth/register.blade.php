8d033ea0a330032894c80009710a8fa77a8fe332
estebanalarconv <estebanalarconv@gmail.com>
2017-06-20 00:59:19 -0400

    arreglo vista registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,27 +5,56 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Registro</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                        <div class="form-group{{ $errors->has('usuario') ? ' has-error' : '' }}">
+                            <label for="usuario" class="col-md-4 control-label">Nombre</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                           <div class="col-md-6">
+                                <input id="usuario" type="text" class="form-control" name="usuario" value="{{ old('usuario') }}" required autofocus>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('usuario'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('usuario') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('run') ? ' has-error' : '' }}">
+                            <label for="run" class="col-md-4 control-label">RUN</label>
+
+                            <div class="col-md-6">
+                                <input id="run" type="text" class="form-control" name="run" value="{{ old('run') }}">
+
+                                @if ($errors->has('run'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('run') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('telefono') ? ' has-error' : '' }}">
+                            <label for="telefono" class="col-md-4 control-label">Telefono</label>
+
+                            <div class="col-md-6">
+                                <input id="telefono" type="text" class="form-control" name="telefono" value="{{ old('telefono') }}">
+
+                                @if ($errors->has('telefono'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('telefono') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -57,6 +86,23 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('tipo_usuario') ? ' has-error' : '' }}">
+                            <label for="tipo_usuario" class="col-md-4 control-label">Tipo de usuario</label>
+
+                            <div class="col-md-6">
+                                 <select name="tipo_usuario">
+                                  <option value="1">Miembro Comunidad</option>
+                                  <option value="2">Organización</option>
+                                </select> 
+
+                                @if ($errors->has('tipo_usuario'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('tipo_usuario') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
