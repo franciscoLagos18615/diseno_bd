900284f0393a15444c319083215068c42205e658
psykosoldierz <psykosoldierz>
2017-06-25 17:18:33 -0400

    Catastrofes con CRUD Completo

---
+++ resources/views/catastrofes/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+
+@section('content')
+
+	<div class="col-sm-8">
+		
+		<h2>
+		Editar Producto
+			<a href="{{ route('catastrofes.index') }}"" class="btn btn-default pull-right">Editar</a>
+		</h2>
+
+			@include('catastrofes.fragment.error')
+
+
+			{!! Form::model($catastrofe, ['route' => ['catastrofes.update', $catastrofe->id], 'method' => 'PUT']) !!}
+
+				@include('catastrofes.fragment.form')
+
+
+			{!! Form::close() !!}
+	</div>
+	<div class="col-sm-4">
+		<!--Comentario en HTML -->
+		@include('catastrofes.fragment.aside')
+	</div>
+
+@endsection
